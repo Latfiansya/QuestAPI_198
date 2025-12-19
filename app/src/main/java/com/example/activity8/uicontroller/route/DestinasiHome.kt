c58197a69e9c8c72e7be5d22e9e6f6d8a058fbf9
Latfiansya Dzikri - 198 <latfiansyadzikri@gmail.com>
2025-12-19 23:24:59 +0700

    refactor: Update R class import in DestinasiHome
    Changes the import for the `R` class from `androidx.compose.material3.R` to `com.example.activity8.R` to resolve the string resource correctly.

--- app/src/main/java/com/example/activity8/uicontroller/route/DestinasiHome.kt
+++ app/src/main/java/com/example/activity8/uicontroller/route/DestinasiHome.kt
@@ -1,11 +1,8 @@
 package com.example.activity8.uicontroller.route
 
-import androidx.compose.material3.R
+import com.example.activity8.R
 
 object DestinasiHome : DestinasiNavigasi {
     override val route = "home"
     override val titleRes = R.string.app_name
-}
-
-{
 }
