3c3494ea8000bab7c4b5c7425d99397d1a6a137e
Latfiansya Dzikri - 198 <latfiansyadzikri@gmail.com>
2025-12-19 23:53:12 +0700

    feat: Add navigation graph for the application
    Introduces the navigation graph using Jetpack Compose Navigation.
    
    - Defines a `NavHost` composable named `HostNavigasi`.
    - Sets up two destinations: `DestinasiHome` for the home screen and `DestinasiEntry` for the data entry screen.
    - The `HomeScreen` can navigate to the `EntrySiswaScreen`.
    - The `EntrySiswaScreen` can navigate back to the `HomeScreen`.

--- app/src/main/java/com/example/activity8/uicontroller/PetaNavgasi.kt
+++ app/src/main/java/com/example/activity8/uicontroller/PetaNavgasi.kt
@@ -1,2 +1,42 @@
 package com.example.activity8.uicontroller
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.example.activity8.uicontroller.route.DestinasiEntry
+import com.example.activity8.uicontroller.route.DestinasiHome
+import com.example.activity8.view.EntrySiswaScreen
+import com.example.activity8.view.HomeScreen
+
+@Composable
+fun DataSiswaApp(navController: NavHostController = rememberNavController(),
+                 modifier: Modifier) {
+    HostNavigasi(navController = navController)
+}
+
+@Composable
+fun HostNavigasi(
+    navController: NavHostController,
+    modifier: Modifier = Modifier
+){
+    NavHost(navController = navController, startDestination = DestinasiHome.route,
+        modifier = Modifier ) {
+
+        composable(DestinasiHome.route) {
+            HomeScreen(navigateToItemEntry = { navController.navigate(DestinasiEntry.route) },
+                navigateToItemUpdate = {
+                    // navController.navigate("${DestinasiDetail.route}/$it")
+                }
+            )
+        }
+
+        composable(DestinasiEntry.route){
+            EntrySiswaScreen(navigateBack = { navController.navigate(DestinasiHome.route) })
+        }
+    }
+}
