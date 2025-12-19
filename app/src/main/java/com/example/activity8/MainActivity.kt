3d51e2b0bac0c48b23835afcd88b78b974047582
Latfiansya Dzikri - 198 <latfiansyadzikri@gmail.com>
2025-12-19 23:43:07 +0700

    feat: Integrate DataSiswaApp into MainActivity
    Replaces the placeholder `Greeting` composable in `MainActivity` with the `DataSiswaApp` composable, setting it as the main content view for the application.

--- app/src/main/java/com/example/activity8/MainActivity.kt
+++ app/src/main/java/com/example/activity8/MainActivity.kt
@@ -20,8 +20,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             Activity8Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    DataSiswaApp(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
