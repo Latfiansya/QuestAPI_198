d70ff93549498dcaab57bea64c3cdf36e72ff6aa
Latfiansya Dzikri - 198 <latfiansyadzikri@gmail.com>
2025-12-27 20:20:25 +0700

    feat: Add Detail and Edit ViewModel providers
    Adds initializers for `DetailViewModel` and `EditViewModel` to the `PenyediaViewModel.Factory`.
    
    These additions enable the creation of `DetailViewModel` and `EditViewModel` instances with `SavedStateHandle` and the data repository.

--- app/src/main/java/com/example/activity8/viewmodel/provider/PenyediaViewModel.kt
+++ app/src/main/java/com/example/activity8/viewmodel/provider/PenyediaViewModel.kt
@@ -6,17 +6,29 @@ import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.activity8.repositori.AplikasiDataSiswa
+import com.example.activity8.viewmodel.DetailViewModel
+import com.example.activity8.viewmodel.EditViewModel
 import com.example.activity8.viewmodel.EntryViewModel
 import com.example.activity8.viewmodel.HomeViewModel
 
 fun CreationExtras.aplikasiDataSiswa(): AplikasiDataSiswa = (
         this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as AplikasiDataSiswa
         )
-
 object PenyediaViewModel {
     val Factory = viewModelFactory {
         initializer { HomeViewModel(aplikasiDataSiswa().containerApp.repositoryDataSiswa) }
-
         initializer { EntryViewModel(aplikasiDataSiswa().containerApp.repositoryDataSiswa) }
+        initializer {
+            DetailViewModel(
+                this.createSavedStateHandle(),
+                aplikasiDataSiswa().containerApp.repositoryDataSiswa
+            )
+        }
+        initializer {
+            EditViewModel(
+                this.createSavedStateHandle(),
+                aplikasiDataSiswa().containerApp.repositoryDataSiswa
+            )
+        }
     }
 }
